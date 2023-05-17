22efa1bc35742d06518b79f1affdad4c09241c3c
Dan Alexandra-Gabriela <danalexandra1211@gmail.com>
2023-05-18 01:01:26 +0300

    remove comments

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 var skillsEl = document.getElementById("skills-list");
 
-// skillsEl.innerHTML += '<li class="favorite">HTML</li>';
-// skillsEl.innerHTML += '<li class="favorite">CSS</li>';
-// skillsEl.innerHTML += '<li class="favorite">JS</li>';
-// skillsEl.innerHTML += '<li class="favorite">Microsoft Office</li>';
-
 var skills = ["HTML", "CSS", "JS", "Microsoft Office"];
-// skillsEl.innerHTML += "<li>" + skills[0] + "</li>";
-// skillsEl.innerHTML += "<li>" + skills[1] + "</li>";
-// skillsEl.innerHTML += "<li>" + skills[2] + "</li>";
-// skillsEl.innerHTML += "<li>" + skills[3] + "</li>";
 
 var skillsHTML = "";
 var i = 0;
