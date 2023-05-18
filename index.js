9bbb2fdb562bf5e0c0525b58d535cd04e26bd660
Dan Alexandra-Gabriela <danalexandra1211@gmail.com>
2023-05-18 14:55:44 +0300

    easy functions

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ for (var i = 0; i < skills.length; i++) {
   skillsEl.innerHTML = skillsHTML;
 }
 
-function displayHome() {
-  document.getElementById("home").style.display = "block";
+function hideAllPages() {
+  document.getElementById("home").style.display = "none";
   document.getElementById("skills").style.display = "none";
   document.getElementById("volunteering").style.display = "none";
   document.getElementById("languages").style.display = "none";
@@ -15,79 +15,42 @@ function displayHome() {
   document.getElementById("hobbies").style.display = "none";
   document.getElementById("contact").style.display = "none";
   document.getElementById("future-projects").style.display = "none";
+}
+
+function displayHome() {
+  hideAllPages();
+  document.getElementById("home").style.display = "block";
 }
 function displaySkills() {
-  document.getElementById("home").style.display = "none";
+  hideAllPages();
   document.getElementById("skills").style.display = "block";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("certification").style.display = "none";
-  document.getElementById("volunteering").style.display = "none";
-  document.getElementById("hobbies").style.display = "none";
-  document.getElementById("contact").style.display = "none";
-  document.getElementById("future-projects").style.display = "none";
 }
 function displayLanguages() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
+  hideAllPages();
   document.getElementById("languages").style.display = "block";
-  document.getElementById("certification").style.display = "none";
-  document.getElementById("volunteering").style.display = "none";
-  document.getElementById("hobbies").style.display = "none";
-  document.getElementById("contact").style.display = "none";
-  document.getElementById("future-projects").style.display = "none";
 }
 function displayCertification() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
+  hideAllPages();
   document.getElementById("certification").style.display = "block";
-  document.getElementById("volunteering").style.display = "none";
-  document.getElementById("hobbies").style.display = "none";
-  document.getElementById("contact").style.display = "none";
-  document.getElementById("future-projects").style.display = "none";
 }
 
 function displayVolunteering() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("certification").style.display = "none";
+  hideAllPages();
   document.getElementById("volunteering").style.display = "block";
-  document.getElementById("hobbies").style.display = "none";
-  document.getElementById("contact").style.display = "none";
-  document.getElementById("future-projects").style.display = "none";
 }
 
 function displayFutureProjects() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("certification").style.display = "none";
-  document.getElementById("volunteering").style.display = "none";
+  hideAllPages();
   document.getElementById("future-projects").style.display = "block";
-  document.getElementById("hobbies").style.display = "none";
-  document.getElementById("contact").style.display = "none";
 }
 
 function displayHobbies() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("certification").style.display = "none";
-  document.getElementById("volunteering").style.display = "none";
-  document.getElementById("future-projects").style.display = "none";
+  hideAllPages();
   document.getElementById("hobbies").style.display = "block";
-  document.getElementById("contact").style.display = "none";
 }
 
 function displayContact() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("certification").style.display = "none";
-  document.getElementById("volunteering").style.display = "none";
-  document.getElementById("future-projects").style.display = "none";
-  document.getElementById("hobbies").style.display = "none";
+  hideAllPages();
   document.getElementById("contact").style.display = "block";
 }
 
